cf0ca97036f3d51caa25b8194267856bbc54f1c4
yatna <yatnavermaa@gmail.com>
2016-03-08 02:32:47 +0530

    Store button displays appropriate message.

--- PowerUp/app/src/main/java/powerup/systers/com/CompletedSceneActivity.java
+++ PowerUp/app/src/main/java/powerup/systers/com/CompletedSceneActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class CompletedSceneActivity extends Activity {
 
@@ -14,6 +15,7 @@ public class CompletedSceneActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.completed_scene);
 		Button backToMap = (Button) findViewById(R.id.ContinueButtonMap);
+		Button store= (Button)findViewById(R.id.store_button);
 		backToMap.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -21,6 +23,12 @@ public class CompletedSceneActivity extends Activity {
 						MapActivity.class);
 				finish();
 				startActivityForResult(myIntent, 0);
+			}
+		});
+		store.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(CompletedSceneActivity.this, "This functionality has not been implemented yet.", Toast.LENGTH_SHORT).show();
 			}
 		});
 	}
